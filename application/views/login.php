0a5dfe6932ff73b61b08b846e6489f3c1aae2b6e
Ramodk <42975445+Ramodk@users.noreply.github.com>
2021-05-08 15:06:08 +0530

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Sign In - KNC Business Management System</title>
+    <title>Sign In - vTwoEnterpries Business Management System</title>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.1/js/all.js"></script>
 
